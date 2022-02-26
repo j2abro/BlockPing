500656d889da40568761b3c41d00e571c57e3f26
John Abraham <j2abro@gmail.com>
2022-02-25 16:31:42 -0800

    base web3 added

--- App.tsx
+++ App.tsx
@@ -27,6 +27,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import './shim'
+
+
+
 const Section: React.FC<{
   title: string;
 }> = ({children, title}) => {
@@ -68,25 +72,11 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+<Text>Hello world</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
